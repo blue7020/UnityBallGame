77bcab0c58f5af57ee5a2e4a4cc5cdf303a87231
blue6050 <eublue7020@gmail.com>
2020-04-28 16:54:02 +0900

    2d Test 이동 구현, animation UI 스크롤 작업

---
+++ 2D Test/Assets/Script/AttackArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackArea : MonoBehaviour
+{
+    [SerializeField]
+    private Player mPlayer;
+    private float mDamage;
+    //List<GameObject> EnteredArr;
+
+    public void SetDamage(float value)
+    {
+        mDamage = value;
+        //EnteredArr = new List<GameObject>();
+    }
+
+    public void Attack()
+    {
+        gameObject.SetActive(true);
+    }
+
+    
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Enemy"))
+    //    {
+    //    collision.gameObject.GetComponent<Enemy>().Hit(mDamage);
+    //    }
+    //}
+    // Start is called before the first frame update
+}

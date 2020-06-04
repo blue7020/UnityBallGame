7c52aa709191af42e0f82f563bb37b38d539894c
blue6050 <edublue7020@gmail.com>
2020-06-05 03:34:56 +0900

    토스트 아포칼립스 랜덤 맵 구현중-실패

---
+++ Clicker/ToastApocalypse/Assets/Script/RoomTemplates.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject Room;
+    public List<GameObject> rooms;
+
+    public float waitTime;
+    public bool spawnedBoss;
+    public GameObject boss;
+
+    private void Update()
+    {
+        if (spawnedBoss == false)
+        {
+            if (waitTime <= 0)
+            {
+                for (int i = 0; i < rooms.Count; i++)
+                {
+                    if (i == rooms.Count - 1)
+                    {
+                        Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                        spawnedBoss = true;
+                    }
+                }
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        
+    }
+}

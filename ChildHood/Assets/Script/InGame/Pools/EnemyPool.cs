3db4a296f38c7dd4224ca7570c1731f7e669145b
blue6050 <edublue7020@gmail.com>
2020-06-20 01:09:01 +0900

    미믹 위치 버그 해결, 해야할 것 : 일반 몬스터와 스테이지 보스 제작

--- ChildHood/Assets/Script/InGame/Pools/EnemyPool.cs
+++ ChildHood/Assets/Script/InGame/Pools/EnemyPool.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class EnemyPool : ObjectPool<Enemy>
 {
+    [SerializeField]
+    public Transform mPos;
+
     private void Awake()
     {
         PoolSetup();
+    }
+
+    protected override Enemy CreateNewObj(int id)
+    {
+        Enemy newObj = Instantiate(mOriginArr[id], mPos);
+        mPools[id].Add(newObj);
+        return newObj;
     }
 }

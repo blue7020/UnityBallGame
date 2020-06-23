8fd15a274f339f63354442b357b450135a0fed6f
blue6050 <edublue7020@gmail.com>
2020-06-23 14:58:29 +0900

    상점 버그 수정 완료, 현재 버그: 버프 적용이 안됨.

---
+++ ChildHood/Assets/Script/InGame/ItemBuy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBuy : MonoBehaviour
+{
+    [SerializeField]
+    private bool Sell;
+
+    public UsingItem item;
+    [SerializeField]
+    private GameObject ItemBowl;
+
+    private void Start()
+    {
+        Sell = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (Sell == false)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (Player.Instance.mInfoArr[Player.Instance.mID].Gold >= item.mInfoArr[item.mID].Price)
+                {
+                    Sell = true;
+                    Player.Instance.mInfoArr[Player.Instance.mID].Gold -= item.mInfoArr[item.mID].Price;
+                    item = Instantiate(item, Player.Instance.gameObject.transform);
+                    item.gameObject.SetActive(false);
+                    Player.Instance.NowItem = item;
+                    UIController.Instance.ShowItemImage();
+                    ItemBowl.gameObject.SetActive(false);
+                    Debug.Log("구입하였습니다!" + item);
+                }
+                else
+                {
+                    Debug.Log("돈이 부족합니다!");
+                }
+
+            }
+        }
+    }
+}
--- ChildHood/Assets/Script/InGame/ShopController.cs
+++ ChildHood/Assets/Script/InGame/ShopController.cs
@@ -6,50 +6,27 @@ public class ShopController : MonoBehaviour
 {
 
     [SerializeField]
-    public Transform mPos;
-
+    public Transform[] mPos;
+    [SerializeField]
+    private ItemBuy[] itembuy;
     [SerializeField]
     private ItemPool mItempool;
-    [SerializeField]
-    private GameObject ItemBowl;
 
     private UsingItem item;
 
-    [SerializeField]
-    private bool Sell;
-
     private void Start()
     {
-        Sell = false;
-        int rand = Random.Range(0, 3);//TODO 이후 아이템 수만큼 수정
-        item = Instantiate(mItempool.GetFromPool(rand), mPos);
-        item.IsShopItem = true;
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (Sell==false)
+        for (int i=0; i<mPos.Length;i++)
         {
-            if (other.gameObject.CompareTag("Player"))
-            {
-                if (Player.Instance.mInfoArr[Player.Instance.mID].Gold >= item.mInfoArr[item.mID].Price)
-                {
-                    //불러와진 아이템 풀의 물품 구입
-                    Sell = true;
-                    Player.Instance.mInfoArr[Player.Instance.mID].Gold -= item.mInfoArr[item.mID].Price;
-                    item = Instantiate(item,Player.Instance.gameObject.transform);
-                    item.gameObject.SetActive(false);
-                    Player.Instance.NowItem = item;
-                    UIController.Instance.ShowItemImage();
-                    ItemBowl.gameObject.SetActive(false);
-                    Debug.Log("구입하였습니다!");
-                }
-                else
-                {
-                    Debug.Log("돈이 부족합니다!");
-                }
-
-            }
+            int rand = Random.Range(0, 3);//TODO 이후 아이템 수만큼 수정
+            item = mItempool.GetFromPool(rand);
+            item.transform.SetParent(mPos[i]);
+            item.transform.position = mPos[i].transform.position;
+            item.IsShopItem = true;
+            itembuy[i].item = item;
+            item = null;
         }
     }
+
+    
 }

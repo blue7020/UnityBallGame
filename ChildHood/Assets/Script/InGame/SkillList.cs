3e354330240b7abb13b3079a965b4ee38d42163f
blue6050 <edublue7020@gmail.com>
2020-07-25 17:12:50 +0900

    플레이어 스킬 및, 유물 기능 만들기 참고: Clicker의 PlayerUpgradeController 에서 델리게이트 참고

---
+++ ChildHood/Assets/Script/InGame/SkillList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillList : MonoBehaviour
+{
+    public static SkillList Instance;
+
+    private void Awake()
+    {
+        if (Instance==null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Tumble()
+    {
+
+    }
+}
